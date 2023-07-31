8b5ea2be03f6a4ff79892631345316ebb7cac65b
PASEDA-MODUPE <127091710+PASEDA-MODUPE@users.noreply.github.com>
2023-07-31 14:20:17 +0100

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,142 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile1.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Modupe paseda ",
+  tagline: " i'm a web developer",
+  img: profile,
+  about: `"Aspiring web developer on a journey to learn and grow in the world of coding and design. Eager to showcase my first steps in front-end development, utilizing HTML, CSS, and JavaScript to bring ideas to life. Excited to be part of this dynamic field and keen to collaborate on new projects. Browse my portfolio to witness my progress and witness the passion I bring to each endeavor."`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/paseda-modupe-056094236",
+  github: "https://github.com/PASEDA-MODUPE?tab=repositories",
+  twitter: "https://twitter.com/oluwaseun101?t=LwXTFpPsiLhMA-bXaIhA&s=09",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Sail Tech `,
+    Location: "Nigeria",
+    Duration: "feb 2023 - present",
+  },
+  {
+    Position: "Trainee IT Support Engineer",
+    Company: `CWG A cademy`,
+    Location: "Nigeria",
+    Type: "Trainee",
+    Duration: "Sep 2022",
+  },
+  
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Software Development",
+    Company: "Sail Tech, Tech4dev",
+    Location: "physical",
+    Type: "Full Time",
+    Duration: "feb 2023",
+  },
+  {
+    Position: "IT Support",
+    Company: "CWG Academy",
+    Location: "physical",
+    Duration: "sep 2022",
+  },
+  
+  
+  {
+    Position: "Hospitality Management Technology",
+    Company: `Lagos State Polythecnic`,
+    
+    Location: "Nigeria",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  tailwind: tailwind,
+  vscode: vscode,
+  npm: npm,
+  git: git,
+  github: github,
+
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    // image: projectImage3,
+    description: `This App Generates profile card randomly`,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://github.com/PASEDA-MODUPE/profile-card",
+  },
+  {
+    title: "Project title 2",
+    // image: projectImage1,
+    description: `This is a project that helps calculate tips given by the customer `,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://github.com/PASEDA-MODUPE/Tips-calculator",
+  },
+  {
+    title: "Project title 3",
+    description: `Search for any city in the world and get the temperature, humidity of the country .`,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://paseda-modupe.github.io/weather-App/",
+  },
+  
+  {
+    title: "Project title 4",
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://paseda-modupe.github.io/Battery-API/",
+  },
+  {
+    title: "Project title 5",
+    description: `The app that lets you search for millions of words  and gives you the accurate meanings.`,
+    techstack: "HTML/CSS, JavaScript",
+    githubLink: "https://paseda-modupe.github.io/Dictionary/",
+  },
+  
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "pasedasunday@gmail.com",
+  phone: "07013213898",
+ 
+};
